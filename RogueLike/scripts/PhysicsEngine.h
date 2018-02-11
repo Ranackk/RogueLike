c3a8a29d9d662c1799942e7aeeebaf7891056ec1
Jan Gihr <j.gihr@online.de>
2018-02-11 16:17:42 +0100

    * Changed the collsions layer namespace to a class for ... reasons

--- RogueLike/scripts/PhysicsEngine.h
+++ RogueLike/scripts/PhysicsEngine.h
@@ -3,12 +3,13 @@
 #include "BoxCollider.h"
 #include "CircleCollider.h"
 
-namespace CollisionLayer {
+class CollisionLayer {
+public:
 	static const unsigned char NONE = 0b00000000;
 	static const unsigned char MAP = 0b00000001;
 	static const unsigned char FRIENDLY = 0b00000010;
 	static const unsigned char HOSTILE = 0b00000100;
-}
+};
 
 class PhysicsEngine
 {
