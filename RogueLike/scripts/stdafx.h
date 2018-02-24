bd9158879b8630436964b6e8f7de5799a9b62236
Jan Gihr <j.gihr@online.de>
2018-02-24 21:58:31 +0100

    - Removed unsused dependencies!

--- RogueLike/scripts/stdafx.h
+++ RogueLike/scripts/stdafx.h
@@ -6,17 +6,12 @@
 #include <stdlib.h>
 #include <iostream>
 #include <list>
-//#include <SFML/System/Vector2.hpp>
 
 #include <memory>
 #include <vector>
 #include <map>
 
-//#include <SFML/Graphics.hpp>
-//#include <SFML\OpenGL.hpp>
-
 #include <GL/glew.h>
-//#include <GL/freeglut.h>
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 
